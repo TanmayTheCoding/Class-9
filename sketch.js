2804a99b74315ba229f099252af780115cbefc5c
TanmayTheCoding <110655848+TanmayTheCoding@users.noreply.github.com>
2022-08-05 11:49:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+
+var ball;
+
+
+
+
+function setup() {
+  createCanvas(400, 400);
+
+  ball = createSprite(200, 200, 60, 60)
+
+}
+
+function draw() {
+  background(30);
+
+  if (keyDown(RIGHT_ARROW)) {
+    ball.x = ball.x + 2
+  }
+
+  if(keyDown("left")){
+    ball.x = ball.x - 2
+
+  }
+
+  if(keyDown("up")){
+    ball.y = ball.y - 2
+  }
+
+  if(keyDown("down")){
+    ball.y = ball.y + 2
+  }
+  drawSprites()
+}
+
+
+
+
